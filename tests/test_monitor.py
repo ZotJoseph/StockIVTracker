23cbfe63925793983a8bf7bbb9d4718907124c9b
ZotJoseph <josel35@uci.edu>
2026-09-12 22:25:12 -0700

    changed test case mocks'

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,7 @@
 import unittest
 from datetime import timedelta, datetime
+from typing import Any, Generator
+
 import Engine
 from Engine.stock_monitor import StockMonitor
 from API.compositeIVFinder import SchwabIV, CompositeIVResult
@@ -12,21 +14,27 @@ Engine.databaseUpdater.DATABASE_NAME = "test_stocks.db"
 Engine.stock_monitor.OPTION_SYMBOLS_PATH = "tests/testSymbols.txt"
 
 
-def patch_fetch_composite_iv(mocker):
+def patch_fetch_composite_iv(mocker, iv_list: list[float]):
     """
     patches API, mock a few returns
     PLEASE REMEMBER TO UPDATE TO composite_iv_result = "interpolated" ELSE THE MONITOR WILL THROW EXCEPTION
+
+    *iv is the list of iv that will be returned in CompositeIVResult, the top of the list will be outputted first
+
+    NOT DESIGNED TO GO BEYOND LIMIT of len(iv_list)
     """
-    mocker.patch("API.compositeIVFinder.SchwabIV.fetch_composite_iv",
-                 side_effect = [
-                     CompositeIVResult(symbol = "GOOGL",
-                                       datetime = datetime.now() - timedelta(
-                                           minutes = 15), iv = 0.1, status = "interpolated"),
-                     CompositeIVResult(symbol = "GOOGL",
-                                       datetime = datetime.now() - timedelta(
-                                           minutes = 0), iv = 0.5, status = "interpolated"),
-                 ]
-                 )
+
+    def composite_iv_result_generator() -> Generator[CompositeIVResult, Any, None]:
+        minutes_before_present = 75
+        for iv in iv_list:
+            yield CompositeIVResult(symbol = "GOOGL",
+                                    datetime = datetime.now() - timedelta(
+                                        minutes = minutes_before_present), iv = iv, status = "interpolated")
+
+
+    iv_results = composite_iv_result_generator()
+    mocker.patch("API.compositeIVFinder.SchwabIV.fetch_composite_iv", side_effect = iv_results)
+
 
 def test_iv_updates(mocker):
     """
@@ -34,23 +42,23 @@ def test_iv_updates(mocker):
     :return:
     """
     # SchwabIV, the class used to access APi and find IV, is being mocked
-    patch_fetch_composite_iv(mocker)
+    patch_fetch_composite_iv(mocker, [.1, .5])
 
-    #wipe database
+    # wipe database
     monitor = StockMonitor()
     wipe_db(monitor.database)
 
-    #first start
+    # first start
     monitor = StockMonitor()
     assert monitor.min_iv["GOOGL"] is None and monitor.max_iv["GOOGL"] is None
     monitor.monitor()
 
-    #restart monitor
+    # restart monitor
     monitor = StockMonitor()
     assert monitor.min_iv["GOOGL"] == .1 and monitor.max_iv["GOOGL"] == .1
     monitor.monitor()
 
-    #restart monitor
+    # restart monitor
     monitor = StockMonitor()
     assert monitor.min_iv["GOOGL"] == .5 and monitor.max_iv["GOOGL"] == .5
 
--- tests/test_test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import pytest
-
-def test_save_user(mocker):
-    mock_conn = mocker.patch("sqlite3.connect")
-    mock_cursor = mock_conn.return_value.something.return_value
