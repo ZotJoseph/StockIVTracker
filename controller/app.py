60497a870292da2163742c4894eb9bbfa26b22d7
ZotJoseph <josel35@uci.edu>
2026-09-17 23:28:40 -0700

    added insert at date for fastAPI

--- controller/app.py
+++ controller/app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from click import DateTime
 from fastapi import FastAPI
 
@@ -12,8 +14,25 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/stocks")
+@app.get("/stocks/{symbol}")
 def read_stock_symbol(symbol: str) -> float:
     database = common.database.DatabaseConnection()
     return database.getLatestIVFromStock(symbol)
+
+
+@app.put("/stocks/{symbol}")
+def update_stock(symbol: str, iv : float, iv_date : datetime.date):
+
+    database = common.database.DatabaseConnection()
+    database.updateDaily(symbol, iv, iv_date)
+
+    return {
+        "symbol": symbol,
+        "composite_30_iv" : iv,
+        "date": datetime.date
+    }
+#TODO: get stock at date
+#TODO: update stock at date
+
+#TODO: get all of a stock within 30 days
 
