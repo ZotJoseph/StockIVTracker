fea165e5aca19131e7549b10fca8e32c22b1e7d0
ZotJoseph <josel35@uci.edu>
2026-09-18 21:34:33 -0700

    added daemon/scheduler via apiScheduler and exchange_calendar, not yet tested

--- main.py
+++ main.py
@@ -2,29 +2,15 @@
 from time import sleep
 from datetime import datetime, timedelta
 
+from controller.scheduler import start_daemon
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from controller.stock_iv_monitor import StockMonitor
 
+import controller.scheduler
 
-MINUTES_PER_MONITOR = 15
-
-
-monitor = StockMonitor()
-
-def start_monitoring():
-
-    next_update = datetime.now()
-    #TODO: dynamically control when to monitor: only when market is open, and reset everyday
-    while True:
-        now = datetime.now()
-        if now >= next_update:
-            print("process ran at " + str(now))
-            next_update = now + timedelta(minutes = MINUTES_PER_MONITOR)
-            monitor.monitor()
-        sleep(55)
 
 if __name__ == '__main__':
-    start_monitoring()
+    start_daemon()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
