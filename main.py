06850a1813c0ec8c5844cae673f3a1c352c44719
ZotJoseph <josel35@uci.edu>
2026-09-18 22:35:45 -0700

    Added logging functionality (not loguru, python built in logging library)

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import threading
 import uvicorn
 
-from controller.scheduler import start_daemon
-from controller.app import app
+
+from common.scheduler import start_daemon
+from common.app import app
+
+import logging
+from common.logging import setup_logging
+setup_logging()
 
 
 def run_server():
     uvicorn.run(app, host = "0.0.0.0", port = 8000)
 
 if __name__ == '__main__':
-
         daemon_thread = threading.Thread(
             target = start_daemon,
             daemon = True
